665d906dc960b9c0b31181d0c3122bfb6db090fa
vbojcev <vbojcev@uwaterloo.ca>
2021-10-01 21:14:07 -0400

    Finished exp.cpp

--- exp.cpp
+++ exp.cpp
@@ -1,39 +1,41 @@
-#include <cmath>
+#include <cmath>  //Power
 #include <iostream>
-#include <string>
-#include <time.h>
+#include <string>  //stoi() function
+#include <time.h>  //Random seed
 
 using namespace std;
 
-float lam;
-float U = 1;
-float dataSet[1000];
-float x;
-float sum = 0;
-float squareSum = 0;
+float lam;  //Exponential parameter
+float U =;  //Uniform random variable on [0,1]
+float dataSet[1000];  //Array of outputs
+float x;  //Exponential random variable
+float sum = 0;  //For calculating mean
+float squareSum = 0;  //For calculating the variance
 float mean;
 float squareMean;
 float variance;
 
 int main(int argc, char* argv[]) {
 
-cout << argv[0] << endl;
-
   if (argc != 2 ) {
+  
     cout << "Invalid input. Please enter lambda and ONLY lambda" << endl;
     return 1;
+    
   } else {
+  
+    cout << "Lambda chosen: " << argv[1] << endl;
   
     lam = stof(argv[1]);
     
     cout << "lambda is: " << lam << endl;
     
-    srand((unsigned)time(nullptr));  //the seed is the current time.
+    srand((unsigned)time(nullptr));  //the seed is the current time. Only needs to be done once.
     
     for (int i = 0; i < 1000; i++) {
     
-      U = (float)(rand()%1000)/1000;
-      x = -(1/lam)*log(1-U);
+      U = (float)(rand()%1000)/1000;  //Determine a uniform random variable in [0,1]
+      x = -(1/lam)*log(1-U);  //Use the uniform random variable to determine an exponential random variable
       dataSet[i] = x;
       
     }
@@ -43,15 +45,14 @@ cout << argv[0] << endl;
       squareSum += pow(dataSet[i], 2);
     }
     
-    mean = sum/1000;
-    squareMean = squareSum/1000;
+    mean = sum/1000;  //mean of the random variable, or approximately the expected value E[X]
+    squareMean = squareSum/1000;  //Equal to E[(x)^2]
     
-    variance = squareMean - pow(mean, 2);
+    variance = squareMean - pow(mean, 2);  //Var(X) = E[(X^2)] - (E[X])^2
     
     cout << "Mean/Expected Value: " << (float)mean << endl;
     cout << "Variance: " << (float)variance << endl;
   
-    return 0;
   }
 
   return 0;
